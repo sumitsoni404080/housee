b3afbeb93b6824f8f017fd2ab706d6965daf7f17
sumitsoni404080 <sumitsoni404080@gmail.com>
2025-09-09 20:41:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var typed = new Typed(".text", {
+    strings: ["Living Solution", "Hostel & PG Solution", "Mess Solution", "Co-living & Working solution"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
